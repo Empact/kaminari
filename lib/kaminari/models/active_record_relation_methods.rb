9c73f3658fb8c82c0d176dcf31d4cfa546234f51
Ben Woosley <ben.woosley@gmail.com>
2011-10-04 02:06:18 -0700

    Added support for using distinct when getting count of records
    Conflicts:
    
    	lib/kaminari/models/active_record_relation_methods.rb
    	lib/kaminari/models/page_scope_methods.rb

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -17,9 +17,20 @@ module Kaminari
         c = c.reorder nil
         # Remove includes only if they are irrelevant
         c = c.except(:includes) unless references_eager_loaded_tables?
+
+        if distinct_column_name.nil?
+          c = c.count
+        else
+          c = c.count(distinct_column_name, :distinct => true)
+        end
         # .group returns an OrderdHash that responds to #count
-        c = c.count
         c.respond_to?(:count) ? c.count : c
+      end
+
+      # Get the column name used in distinct query.
+      # This could have been set on the Model class, or the ActiveRecord::Relation
+      def distinct_column_name
+        @distinct_column || distinct_column
       end
     end
   end
